f608af443ceea3601814a79c565973ca19cc5ddf
Stephen J Bradshaw <stephen.j.bradshaw@gmail.com>
2020-09-21 11:31:39 +0100

    Add user context

--- src/App.js
+++ src/App.js
@@ -1,30 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
 import { Router } from "@reach/router";
-// import "./App.css";
-// import "./css/problemsList.css";
-// import "./css/problemCard.css";
-// import "./css/singleProblem.css";
-// import "./css/suggestions.css";
-// import "./css/suggestionCard.css";
-// import "./css/button.css";
 import { StyledHome } from "./styled/home";
 import SingleProblem from "./components/singleProblem/SingleProblem";
 import Header from "./components/header/Header";
 import MentorForm from "./components/becomeAMentor/MentorForm";
 import Dashboard from "./components/dashboard/Dashboard";
+import { UserContext } from "./UserContext";
 
-function App() {
-  return (
-    <div className="app">
-      <Header />
-      <Router>
-        <StyledHome path="/" />
-        <MentorForm path="/become-a-mentor" />
-        <SingleProblem path="/problem" />
-        <Dashboard path="/dashboard" />
-      </Router>
-    </div>
-  );
+class App extends Component {
+  state = { username: null };
+
+  toggleLogin = () => {
+    this.setState(({ user }) => {
+      if (user) return { user: null };
+      else return { user: "Destiny82" };
+    });
+  };
+
+  render() {
+    // const err = { type: "general404", msg: "Page not found!", status: 404 };
+    const { user } = this.state;
+
+    return (
+      <div className="app">
+        <UserContext.Provider value={{ user, toggleLogin: this.toggleLogin }}>
+          <Header />
+          <Router>
+            <StyledHome path="/" />
+            <MentorForm path="/become-a-mentor" />
+            <SingleProblem path="/problem" />
+            <Dashboard path="/dashboard" />
+          </Router>
+        </UserContext.Provider>
+      </div>
+    );
+  }
 }
 
 export default App;
