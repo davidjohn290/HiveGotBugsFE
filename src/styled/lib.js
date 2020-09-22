f4c2656b0235c55370d3e9c4d40f4502fd1a71c8
Kojjob <kodeon6@gmail.com>
2020-09-22 10:33:35 +0100

    Styling mentors page

--- src/styled/lib.js
+++ src/styled/lib.js
@@ -4,7 +4,8 @@ import Loader from "../components/Loader";
 import ErrorPage from "../components/ErrorPage";
 
 // <Link>, sometimes overridden as <Button>
-export const StyledHexButton = styled(Link)`
+export const StyledHexButton = styled(Link)
+`
   position: relative;
   width: 70px;
   height: 40.41px;
@@ -44,7 +45,7 @@ export const StyledHexButton = styled(Link)`
   }
 `;
 
-export const StyledDifficultyButton = styled.button`
+export const StyledDifficultyButton = styled.button `
   margin-left: 0.2em;
   margin-right: 0.2em;
   text-align: center;
@@ -60,7 +61,8 @@ export const StyledDifficultyButton = styled.button`
   }};
 `;
 
-export const StyledLink = styled(Link)`
+export const StyledLink = styled(Link)
+`
   text-decoration: none;
   color: black;
   font-weight: 500;
@@ -70,10 +72,12 @@ export const StyledLink = styled(Link)`
   }
 `;
 
-export const StyledLoader = styled(Loader)`
+export const StyledLoader = styled(Loader)
+`
   display: flex;
   flex-direction: column;
   align-items: center;
 `;
 
-export const StyledErrorPage = styled(ErrorPage)``;
+export const StyledErrorPage = styled(ErrorPage)
+``;
