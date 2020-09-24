43f78baf77738a6663f44ceee6262c78ae917b51
Kojjob <kodeon6@gmail.com>
2020-09-24 11:38:02 +0100

    --amend

--- src/App.js
+++ src/App.js
@@ -11,35 +11,46 @@ import MentorList from "./components/mentors/MentorList";
 import ErrorPage from "./components/ErrorPage";
 
 class App extends Component {
-  state = { username: null, err: null };
+    state = { username: null, err: null };
 
-  setUsername = (username) => {
-    this.setState({ username });
-  };
+    setUsername = (username) => {
+        this.setState({ username });
+    };
 
-  render() {
-    const err = { type: "general404", msg: "Page not found!", status: 404 };
-    const { username } = this.state;
+    render() {
+        const err = { type: "general404", msg: "Page not found!", status: 404 };
+        const { username } = this.state;
 
-    return (
-      <div className="app">
-        <UserContext.Provider
-          value={{ username, setUsername: this.setUsername }}
-        >
-          <Header />
-          <Router>
-            <StyledHome path="/" />
-            <StyledSingleProblem path="/problem/:problem_id" />
-            <StyledMentorForm path="/become-a-mentor" />
-            <MentorPage path="/:username" />
-            <StyledDashboard path="/dashboard" />
-            <MentorList path="/mentors" />
-            <ErrorPage default {...err} />
-          </Router>
-        </UserContext.Provider>
-      </div>
-    );
-  }
+        return ( <
+            div className = "app" >
+            <
+            UserContext.Provider value = {
+                { username, setUsername: this.setUsername } } >
+            <
+            Header / >
+            <
+            Router >
+            <
+            StyledHome path = "/" / >
+            <
+            StyledSingleProblem path = "/problem/:problem_id" / >
+            <
+            StyledMentorForm path = "/become-a-mentor" / >
+            <
+            MentorPage path = "/:username" / >
+            <
+            StyledDashboard path = "/dashboard" / >
+            <
+            MentorList path = "/mentors" / >
+            <
+            ErrorPage
+            default {...err }
+            /> <
+            /Router> <
+            /UserContext.Provider> <
+            /div>
+        );
+    }
 }
 
 export default App;
